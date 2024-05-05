0e7185436558f60406ef37d26298392cc510f05a
Youssef Bouryal <youssefbouryal02@gmail.com>
2024-05-05 15:25:06 +0100

    puppet script to set ssh config

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# Setting up ssh config for client
+file_line { 'turn of pass auth':
+  ensure => present,
+  path => '/etc/ssh/ssh_config',
+  line => '    PasswordAuthentication no',
+}
+
+file_line { 'declare identity file (pub key)':
+  ensure => present,
+  path => '/etc/ssh/ssh_config',
+  line => '    IdentityFile ~/.ssh/school',
+
+}
